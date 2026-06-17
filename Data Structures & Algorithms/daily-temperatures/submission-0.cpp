000abaf4e9a9ef46494d978a5db2b8149a10e8a7
Mohit Mehto <mohit.m.0311@gmail.com>
2026-06-17 12:24:35 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        stack<int> st;
+        int n = temp.size();
+        vector<int> arr(n,0);
+        for(int i=n-1; i>=0; i--){
+            while(!st.empty() && temp[st.top()]<=temp[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                arr[i] = st.top() - i;
+            }
+            st.push(i);
+        } 
+        return arr;
+    }
+};
